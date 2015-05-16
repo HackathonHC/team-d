712cb82cde2476b48e03fa62f24023931c2291fb
westzakk <yuya.nishizaki@happyelements.co.jp>
2015-05-16 16:45:14 +0900

    DiceButtonを配置しました
    変更点
    - クリックの検出

---
+++ Unity/Assets/Scripts/DiceButton.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DiceButton : MonoBehaviour
+{
+	private UISprite sprite;
+
+	void Awake()
+	{
+		this.sprite = this.transform.Find("Sprite").GetComponent<UISprite>();
+	}
+
+	public void OnButtonClick()
+	{
+		Debug.Log("click!");
+	}
+}
